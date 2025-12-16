9560f032a81cfc186b5d771e88b471133c18442e
Enzo Mendez <enzo.mendez315@gmail.com>
2025-12-16 16:41:13 -0700

    Create database using an engine.

--- app/core/db.py
+++ app/core/db.py
@@ -1,1 +1,11 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
+
+from models import Endpoint
+
+
+DATABASE_URL = ""
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Create the actual database using the engine
+SQLModel.metadata.create_all(engine)
--- app/models.py
+++ app/models.py
@@ -8,7 +8,8 @@ class Endpoint(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     url: str
     name: str | None
-    is_active: bool
+    is_up: bool
+    is_monitored: bool
     status_code: int | None
     last_checked: datetime  # when it was pinged last
     created_at: datetime = Field(default_factory=datetime.now(timezone.utc))
