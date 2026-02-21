02502461b5f966378606e88ca534b80de3206ad4
enzomendez315 <enzo.mendez315@gmail.com>
2026-02-20 21:11:56 -0700

    Post endpoints.

--- internal/endpoint/model.go
+++ internal/endpoint/model.go
@@ -3,10 +3,10 @@ package endpoint
 import "time"
 
 type Endpoint struct {
-	ID        string
-	URL       string
-	Name      string
-	Enabled   bool
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `json:"id"`
+	URL       string    `json:"url"`
+	Name      string    `json:"name"`
+	Enabled   bool      `json:"enabled"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,19 +1,51 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/enzomendez315/sentinel/internal/endpoint"
+	"github.com/google/uuid"
 )
 
 func Run() error {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("Hello World"))
 		if err != nil {
 			fmt.Printf("Error writing response: %v", err)
 		}
 	})
+
+	mux.HandleFunc("POST /endpoints", func(w http.ResponseWriter, r *http.Request) {
+		var ep endpoint.Endpoint
+
+		// Decode JSON into struct
+		err := json.NewDecoder(r.Body).Decode(&ep)
+		if err != nil {
+			http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+			return
+		}
+
+		// Set fields
+		ep.ID = uuid.New().String()
+		ep.CreatedAt = time.Now()
+		ep.UpdatedAt = time.Now()
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+
+		// Encode JSON for response
+		err = json.NewEncoder(w).Encode(ep)
+		if err != nil {
+			fmt.Printf("Error writing response: %v", err)
+		}
+	})
+
+	fmt.Println("Starting server at http://localhost:8080")
 
 	return http.ListenAndServe(":8080", mux)
 }
